1f3908bfb193d243ccb595f44caff98cd35c51fb
Naveed Massjouni <naveedm9@gmail.com>
2010-10-30 20:34:44 -0400

    All methods now require a db argument.

--- lib/CouchDB/Simple.pm
+++ lib/CouchDB/Simple.pm
@@ -11,57 +11,56 @@ sub new {
     my($class, %args) = @_;
     my $host  = $args{host}  || 'localhost';
     my $port  = $args{port}  || 5984;
-    my $db    = $args{db}    || 'sampledb';
     my $agent = $args{agent} || LWP::UserAgent->new();
     my $self = {
         agent    => $agent,
-        base_uri => "http://$host:$port/$db",
+        base_uri => "http://$host:$port",
     };
     bless $self, $class;
     return $self;
 }
 
 sub create_document {
-    my ($self, $id, $doc) = @_;
+    my ($self, $db, $id, $doc) = @_;
     my $json = to_json($doc);
-    my $req = HTTP::Request->new(PUT => "$self->{base_uri}/$id", [], $json);
+    my $req = HTTP::Request->new(PUT => "$self->{base_uri}/$db/$id", [], $json);
     return $self->{agent}->request($req)->content;
 }
 
 sub delete_document {
-    my ($self, $id) = @_;
-    my $json = $self->get_document($id);
+    my ($self, $db, $id) = @_;
+    my $json = $self->get_document($db, $id);
     my $data = from_json($json);
 
     return $json if $data->{error};
 
-    my $uri = "$self->{base_uri}/$id?rev=$data->{_rev}";
+    my $uri = "$self->{base_uri}/$db/$id?rev=$data->{_rev}";
     my $req = HTTP::Request->new(DELETE => $uri);
     return $self->{agent}->request($req)->content;
 }
 
 sub get_document {
-    my ($self, $id) = @_;
-    my $req = HTTP::Request->new(GET => "$self->{base_uri}/$id");
+    my ($self, $db, $id) = @_;
+    my $req = HTTP::Request->new(GET => "$self->{base_uri}/$db/$id");
     return $self->{agent}->request($req)->content;
 }
 
 sub update_document {
-    my ($self, $id, $doc, $merge) = @_;
+    my ($self, $db, $id, $doc, $merge) = @_;
     $merge = 1 unless defined $merge;
 
-    my $data = from_json($self->get_document($id));
+    my $data = from_json($self->get_document($db, $id));
     $doc->{_rev} = $data->{_rev};
     $doc = { %$data, %$doc } if $merge;
 
-    my $req = HTTP::Request->new(PUT => "$self->{base_uri}/$id",
+    my $req = HTTP::Request->new(PUT => "$self->{base_uri}/$db/$id",
         [], to_json($doc));
     return $self->{agent}->request($req)->content;
 }
 
 sub get_all_documents {
-    my ($self) = @_;
-    my $req = HTTP::Request->new(GET => "$self->{base_uri}/_all_docs");
+    my ($self, $db) = @_;
+    my $req = HTTP::Request->new(GET => "$self->{base_uri}/$db/_all_docs");
     return $self->{agent}->request($req)->content;
 }
 
@@ -81,7 +80,6 @@ Example usage:
 
     use CouchDB::Simple
     my $couch = CouchDB::Simple->new(
-        db   => 'some_db',
         host => 'localhost', # defaults to 'localhost'
         port => 5984, # defaults to 5984
     );
--- t/01-basic.t
+++ t/01-basic.t
@@ -13,43 +13,44 @@ my $db = 'sampledb';
 my $doc_id = 'foo_id';
 my $rev = 3;
 my $test_json = to_json({ foo => 'bar', _rev => $rev });
-my $base_uri = "http://$host:$port/$db";
+#my $base_uri = "http://$host:$port/$db";
+my $base_uri = "http://$host:$port";
 
 my $mock_agent = LWP::UserAgent->new();
 my $couch = CouchDB::Simple->new(
     host => $host,
     port => $port,
-    db => $db,
+    #db => $db,
     agent => $mock_agent,
 );
 
 $mock_agent->map(
-    HTTP::Request->new(PUT => "$base_uri/$doc_id", [], $test_json),
+    HTTP::Request->new(PUT => "$base_uri/$db/$doc_id", [], $test_json),
     HTTP::Response->new(200, '', [], $test_json)
 );
 
-is $couch->create_document($doc_id, from_json($test_json)) => $test_json,
+is $couch->create_document($db, $doc_id, from_json($test_json)) => $test_json,
     'create_document works';
 
 $mock_agent->map(
-    HTTP::Request->new(GET => "$base_uri/$doc_id"),
+    HTTP::Request->new(GET => "$base_uri/$db/$doc_id"),
     HTTP::Response->new(200, '', [], $test_json)
 );
 
-is $couch->get_document($doc_id) => $test_json, 'get_document works';
+is $couch->get_document($db, $doc_id) => $test_json, 'get_document works';
 
 $mock_agent->map(
-    HTTP::Request->new(DELETE => "$base_uri/$doc_id?rev=$rev"),
+    HTTP::Request->new(DELETE => "$base_uri/$db/$doc_id?rev=$rev"),
     HTTP::Response->new(200, '', [], $test_json)
 );
 
-is $couch->delete_document($doc_id) => $test_json, 'delete_document works';
+is $couch->delete_document($db, $doc_id) => $test_json, 'delete_document works';
 
 $mock_agent->map(
-    HTTP::Request->new(PUT => "$base_uri/$doc_id", [], $test_json),
+    HTTP::Request->new(PUT => "$base_uri/$db/$doc_id", [], $test_json),
     HTTP::Response->new(200, '', [], $test_json)
 );
 
-is $couch->update_document($doc_id, from_json($test_json)) => $test_json,
+is $couch->update_document($db, $doc_id, from_json($test_json)) => $test_json,
     'update_document works';
 
